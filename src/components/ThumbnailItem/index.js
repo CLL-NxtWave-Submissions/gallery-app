d33066c532df41ef527400bc4afa2b311d32bdc2
Surya T N <suryatej.nim@yahoo.com>
2022-09-07 15:01:39 +0000

    Implemented Thumbnail functional component to render a thumbnail image item and propagate state change to Gallery class component, when clicked/selected by the user. Updated styling of JSX elements in ThumbnailItem component.

--- src/components/ThumbnailItem/index.js
+++ src/components/ThumbnailItem/index.js
@@ -1,1 +1,20 @@
-// Write your code here.
+import './index.css'
+
+const ThumbnailItem = (props) {
+    const {itemData, onItemSelect, isSelectedItem} = props
+    const {thumbnailUrl, thumbnailAltText} = itemData
+
+    const itemStyling = `gallery-thumbnail-item ${isSelectedItem ? 'selected-gallery-thumbnail' : ''}`
+
+    const onClickItem = () => {
+        onItemSelect(itemData)
+    }
+
+    return (
+        <li className={itemStyling} onClick={onClickItem}>
+            <img className="gallery-thumbnail-img" src={thumbnailUrl} alt={thumbnailAltText} />
+        </li>
+    )
+}
+
+export default ThumbnailItem
